c1db0668f73ad5897310460965ff87f355be1b43
sushelex@gmail.com <sushelex@gmail.com>
2019-09-30 16:47:43 +0530

    harpoon

---
+++ harpoon/src/main/java/tweek/harpoon/Tweek1.java
@@ -0,0 +1,10 @@
+package tweek.harpoon;
+
+public class Tweek1 {
+
+	public static void main(String[] args) {
+		System.out.println("this is Tweek1 class main method");
+
+	}
+
+}
---
+++ harpoon/src/test/java/tweek/harpoon/TestTweek.java
@@ -0,0 +1,12 @@
+package tweek.harpoon;
+
+import org.testng.annotations.Test;
+
+public class TestTweek 
+{
+@Test
+public void getName()
+{
+	System.out.println("this is tweek test");
+}
+}
